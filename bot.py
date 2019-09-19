e5afcda4e5b47ea899240ccc9a74d15b70ad815c
Ryan Silva <RyandotSilva@gmail.com>
2019-09-18 20:21:57 -0400

    Add on_ready method to check if bot is ready

--- bot.py
+++ bot.py
@@ -1,4 +1,10 @@
 import discord
 from discord.ext import commands
 
+# Instance of the bot with a prefix.
 client = commands.Bot(command_prefix = '.')
+
+# Check for ready state
+@client.event 
+async def on_ready():
+    print('Bot is ready.')
