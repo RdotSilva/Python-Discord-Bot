4b7504e86bbc562462a9c7a44897b7c9a4f3ad0a
Ryan Silva <RyandotSilva@gmail.com>
2019-09-20 20:59:14 -0400

    Add command to reload cog

--- bot.py
+++ bot.py
@@ -96,6 +96,12 @@ async def load(ctx, extension):
 async def unload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
 
+# Reload a cog.
+@client.command()
+async def reload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+    client.load_extension(f'cogs.{extension}')
+
 # Get all files within cogs directory with .py extension & load the cog.
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
