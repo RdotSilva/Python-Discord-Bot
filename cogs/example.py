fb249ec6526745860c1bb0aa58cde6f8aad01916
Ryan Silva <RyandotSilva@gmail.com>
2019-09-20 20:52:18 -0400

    Add on_ready Cog listener

--- cogs/example.py
+++ cogs/example.py
@@ -5,5 +5,9 @@ class Example(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Bot is online.')
+
 def setup(client):
     client.add_cog(Example(client))
